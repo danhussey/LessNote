eddd0661d28b80735d1b34b259868a65167f3cb2
Daniel Hussey <daniel@hussey.io>
2024-12-25 00:47:53 +1100

    Initial Commit

---
+++ LessNote/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LessNote
+//
+//  Created by Daniel Hussey on 25/12/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LessNote/LessNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  LessNoteApp.swift
+//  LessNote
+//
+//  Created by Daniel Hussey on 25/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct LessNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
